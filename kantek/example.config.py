122ef2407e4993c0141a31ee74ac939599aff2a3
kmpkmp3291 <96038351+kmpkmp3291@users.noreply.github.com>
2022-01-25 20:26:02 +0530

    Update example.config.py

--- kantek/example.config.py
+++ kantek/example.config.py
@@ -12,3 +12,18 @@ log_channel_id: Union[str, int] = ''
 
 # This is regex so make sure to escape the usual characters
 cmd_prefix: str = r'\.'
+from telethon import events
+from telethon.events import NewMessage
+
+from config import cmd_prefix
+from utils.pluginmgr import KantekPlugin
+
+
+class Ping(KantekPlugin):
+    """A short help message for the entire plugin"""
+    name = 'Ping'
+
+    @events.register(NewMessage(outgoing=True, pattern=f'{cmd_prefix}ping'))
+    async def ping(event):
+        """A long message about the command"""
+        await event.reply('Pong 🏓')
